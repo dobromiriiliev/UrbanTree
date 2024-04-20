7b8f4fa60deed9e6584e56d24e4a6ac6ab9009ab
Dobromir <127112391+dobromiriiliev@users.noreply.github.com>
2024-04-20 14:51:57 -0400

    Update 7

---
+++ build/CMakeFiles/UrbanTree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UrbanTree.dir/main.cpp.o"
+  "CMakeFiles/UrbanTree.dir/main.cpp.o.d"
+  "UrbanTree"
+  "UrbanTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UrbanTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/UrbanTree.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for UrbanTree.
+# This may be replaced when dependencies are built.
